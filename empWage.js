564d34c37003caa2299b06990ff8ef43e38c2128
ChandradipMoonlight <chandradipshivankar@gmail.com>
2021-06-24 15:20:24 +0530

    Refactored: code by using function

--- empWage.js
+++ empWage.js
@@ -4,20 +4,20 @@ const PART_TIME_HOUR = 4;
 const FULL_TIME_HOUR = 8;
 const WAGE_PER_HOUR = 20;
 
-let empHr = 0;
-
-let empCheck = Math.floor(Math.random() * 10 ) % 3;
-
-switch (empCheck) {
-    case IS_PART_TIME:
-        empHr = PART_TIME_HOUR;
-        break;
-    case IS_FULL_TIME:
-        empHr = FULL_TIME_HOUR;
-        break;
-    default :
-    empHr = 0;
+function getWorkingHours(empCheck) {
+    switch (empCheck) {
+        case IS_PART_TIME:
+           return PART_TIME_HOUR;
+            break;
+        case IS_FULL_TIME:
+            return FULL_TIME_HOUR;
+            break;
+        default:
+            return 0;
+    }
 }
 
-let empWage = empHr * WAGE_PER_HOUR;
-console.log("Employee wage is :" + empWage)
+let empCheck = Math.floor(Math.random() * 10) % 3;
+let empHrs = getWorkingHours(empCheck);
+let empWage = empHrs * WAGE_PER_HOUR;
+console.log("employee hour is : "+empHrs+" hr\nAnd Employee wage is : " + empWage+" Rs")
