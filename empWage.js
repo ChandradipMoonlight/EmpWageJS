d5752cdc38cfbb25821c72019bab5566ff7c0e3f
ChandradipMoonlight <chandradipshivankar@gmail.com>
2021-06-25 17:56:32 +0530

    Added: Code to store daily wages

--- empWage.js
+++ empWage.js
@@ -6,6 +6,11 @@ const WAGE_PER_HOUR = 20;
 const NUM_OF_WORKING_DAYS = 20;
 const MAX_HRS_IN_MONTH = 160;
 
+/**
+ * this is function to to get working hours daily based on the type of employee.
+ * @param {*} empCheck 
+ * @returns employee hours
+ */
 function getWorkingHours(empCheck) {
     switch (empCheck) {
         case IS_PART_TIME:
@@ -19,13 +24,33 @@ function getWorkingHours(empCheck) {
     }
 }
 
+/**
+ * this is function calculate employee wage according to employee hours
+ * @param {*} empHrs 
+ * @returns employee wage
+ */
+function calculateWage(empHrs) {
+    return WAGE_PER_HOUR * empHrs;
+}
+
 let totalEmpHrs = 0;
 let toatlWorkingDays = 0;
+
+// declaration of array to store daily wage of employee
+let empDailyWageArr = new Array();
+
 while (totalEmpHrs <= MAX_HRS_IN_MONTH && toatlWorkingDays < NUM_OF_WORKING_DAYS) {
     toatlWorkingDays ++;
     let empCheck = Math.floor(Math.random() * 10) % 3;
-    totalEmpHrs += getWorkingHours(empCheck);
+    let empHr = getWorkingHours(empCheck);
+    totalEmpHrs += empHr;
+    empDailyWageArr.push(calculateWage(empHr))
 }
 
-let empWage = totalEmpHrs * WAGE_PER_HOUR;
-console.log("employee hour is : " + totalEmpHrs +" hr\nTotal Working Days is : " + toatlWorkingDays + " \nAnd Employee wage is : " + empWage + " Rs")
+let totalEmpWage = calculateWage(totalEmpHrs); 
+
+console.log(" The total employees hour is : " + totalEmpHrs +" hr\nTotal Working Days is : " + toatlWorkingDays + 
+            " \nAnd Employee wage is : " + totalEmpWage + " Rs")
+
+console.log("Daily employee wages are = " + empDailyWageArr);
+
