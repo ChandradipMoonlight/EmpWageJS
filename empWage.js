2e56a91d7f83ebb043900d2f9280dce7018d09f2
ChandradipMoonlight <chandradipshivankar@gmail.com>
2021-06-26 07:37:30 +0530

    Added: code array helper Function

--- empWage.js
+++ empWage.js
@@ -47,10 +47,110 @@ while (totalEmpHrs <= MAX_HRS_IN_MONTH && toatlWorkingDays < NUM_OF_WORKING_DAYS
     empDailyWageArr.push(calculateWage(empHr))
 }
 
-let totalEmpWage = calculateWage(totalEmpHrs); 
+let empWage = calculateWage(totalEmpHrs); 
 
 console.log(" The total employees hour is : " + totalEmpHrs +" hr\nTotal Working Days is : " + toatlWorkingDays + 
+            " \nAnd Employee wage is : " + empWage + " Rs")
+
+// Array Helper Functions
+
+/**
+ * this function is for do sum of daily employee wage.
+ * @param {*} dailyWage 
+ * @returns sum of employee wage
+ */
+ let totalEmpWage = 0;
+function sum(dailyWage) {
+    return totalEmpWage += dailyWage; 
+}
+
+/**
+ * Calculating total employee wage by using forEach transversal method
+ */
+empDailyWageArr.forEach(sum);
+console.log("\ncalculation of total employee wage by using forEach method:- ")
+console.log("The total employees hour is : " + totalEmpHrs +" hr\nTotal Working Days is : " + toatlWorkingDays + 
             " \nAnd Employee wage is : " + totalEmpWage + " Rs")
 
-console.log("Daily employee wages are = " + empDailyWageArr);
+/**
+ * calculating toatal employee wage by using reduce method.
+ * @param {*} totalWage 
+ * @param {*} dailyWage 
+ * @returns total employe wage.
+ */
+function totalWages(totalWage, dailyWage) {
+    return totalWage + dailyWage;
+}
 
+let totalEmpWageByReduceMethod = empDailyWageArr.reduce(totalWages, 0)
+
+console.log("Total employee Wage with reduce method: " + totalEmpWageByReduceMethod + " Rs");
+
+/**
+ * this is method to store Day along with Daily wage using Array map helper method
+ * @param {*} dailyWage 
+ * @returns 
+ */
+let dailyCounter = 0;
+function mapDayWithWage(dailyWage) {
+    dailyCounter ++;
+    return "Day-" + dailyCounter + "-Wage is = "+ dailyWage;
+}
+
+// Array map method
+let mapDayWithWageArr = empDailyWageArr.map(mapDayWithWage);
+console.log("Daily Wage Map :-");
+console.log(mapDayWithWageArr);
+
+/**
+ * this is method to show full time wage of 160 were earned.
+ * @param {*} dailyWage 
+ * @returns 
+ */
+function fulltimeWage(dailyWage) {
+    return dailyWage.includes("160");
+}
+
+//Use of fileter method to filter wage earned 160
+let fulltimeArr = mapDayWithWageArr.filter(fulltimeWage);
+console.log("Daily Wage Filter When Fulltime Wage Earned");
+console.log(fulltimeArr);
+
+//Find first occurance when Fulltime wage was earned bu using find method
+console.log("First time full time Wage was earned day is : ");
+console.log(mapDayWithWageArr.find(fulltimeWage));
+
+/**
+ * this is method to Check if Every element of full time wage is truly holding full time wage
+ * @param {*} dailyWage 
+ * @returns booean type
+ */
+function isAllFulltimeWage(dailyWage) {
+    return dailyWage.includes("160");
+}
+console.log("Check all element have Full Time Wage:-")
+console.log(fulltimeArr.every(isAllFulltimeWage));
+
+/**
+ * this is method to Check If there is any Part time Wage
+ * @param {*} dailyWage 
+ * @returns 
+ */
+function isAnyPartTimeWage(dailyWage) {
+    return dailyWage.includes("60");
+}
+console.log("Check is any Part time Wage:");
+console.log(mapDayWithWageArr.some(isAnyPartTimeWage));
+
+/**
+ * this is function to find number of days employee worked
+ * @param {*} numOfDays 
+ * @param {*} dailyWage 
+ * @returns 
+ */
+function totalDaysworked(numOfDays, dailyWage) {
+    if (dailyWage>0) return numOfDays+1;
+    return numOfDays;
+}
+console.log("Number of days employee worked:")
+console.log(empDailyWageArr.reduce(totalDaysworked, 0));
